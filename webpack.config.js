ee87765b8da12281cfa8091dcb92bb954245eb66
taiki-fw <taiki_52@outlook.jp>
2021-06-28 00:14:04 +0900

    :sparkles:compile by babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
   optimization: {
     usedExports: true,
   },
